ea120863ece1d6506c0c3ddd59d9308c99939c23
Neha Patil <nehapatil680487@gmail.com>
2026-08-18 19:43:17 +0530

    Time: 4 ms (99.08%), Space: 47.5 MB (93.78%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,37 +1,29 @@
 class Solution {
     public boolean validPalindrome(String s) {
 
-        int left = 0;
-        int right = s.length() - 1;
+       int left=0;
+       int right=s.length()-1;
 
-        while (left < right) {
-
-            if (s.charAt(left) != s.charAt(right)) {
-
-                // Delete left character OR right character
-                return isPalindrome(s, left + 1, right) ||
-                       isPalindrome(s, left, right - 1);
-            }
-
-            left++;
-            right--;
+       while(left < right){
+        if(s.charAt(left) != s.charAt(right)){
+            //delete left character or right character
+            return isPalindrome(s,left+1,right) ||
+                   isPalindrome(s,left,right-1);
         }
-
-        return true;
+        left++;
+        right--;
+       }
+       return true;
     }
 
-    public boolean isPalindrome(String s, int left, int right) {
-
-        while (left < right) {
-
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-
-            left++;
-            right--;
+  public boolean isPalindrome(String s,int left,int right) {
+    while(left < right){
+        if(s.charAt(left) != s.charAt(right)){
+            return false;
         }
-
-        return true;
+        left++;
+        right--;
     }
+    return true;
+  } 
 }
